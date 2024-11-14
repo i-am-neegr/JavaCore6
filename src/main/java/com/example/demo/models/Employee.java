6b228b769a3b5038a2b42439b80e1c0a7cd1ca31
i-am-neegr <martial.artem@gmail.com>
2024-11-14 15:56:26 +0300

    small fixes

--- src/main/java/com/example/demo/models/Employee.java
+++ src/main/java/com/example/demo/models/Employee.java
@@ -6,14 +6,10 @@ public class Employee {
 
     final private String firstName;
     final private String lastName;
-    final private int salary;
-    final private int departmentID;
 
-    public Employee(String firstName, String lastName, int salary, int departmentID) {
+    public Employee(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.salary = salary;
-        this.departmentID = departmentID;
     }
 
     public String getFirstName() {
@@ -24,7 +20,9 @@ public class Employee {
         return lastName;
     }
 
-    public String getFullName() {return firstName + " " + lastName;}
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -39,21 +37,11 @@ public class Employee {
         return Objects.hash(firstName, lastName);
     }
 
-    public int getSalary() {
-        return salary;
-    }
-
-    public int getDepartmentID() {
-        return departmentID;
-    }
-
     @Override
     public String toString() {
         return "Employee{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", salary=" + salary +
-                ", departmentID=" + departmentID +
+                "firstName='" + firstName + " + " +
+                ", lastName='" + lastName + " + " +
                 '}';
     }
 }
--- src/main/java/com/example/demo/services/BaseServiceInterface.java
+++ src/main/java/com/example/demo/services/BaseServiceInterface.java
@@ -1,9 +1,10 @@
 package com.example.demo.services;
 
 import com.example.demo.models.Employee;
+import org.springframework.http.ResponseEntity;
 
-public interface EmployeeServiceInterface {
-    void add(String firstName, String lastName);
+public interface BaseServiceInterface {
+    ResponseEntity<Object> add(String firstName, String lastName);
     void delete(String firstName, String lastName);
     Employee getEmployee(String firstName, String lastName);
 }
